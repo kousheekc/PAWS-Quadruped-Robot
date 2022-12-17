ac01a752f730c329f576f6dee3fe16369e514679
kousheekc <kousheekc@gmail.com>
2022-12-17 19:51:13 +0100

    new whole body controller

--- paws_kinematics/src/dummy_body_pose_pub.py
+++ paws_kinematics/src/dummy_body_pose_pub.py
@@ -26,12 +26,12 @@ if __name__ == '__main__':
     rospy.init_node('dummy_body_pose_pub_node', anonymous=True)
 
     body_p_pub = rospy.Publisher("/body", Pose, queue_size=1)
-    lf_p_pub = rospy.Publisher("/foot/pose_abs/lf", Point, queue_size=1)
-    rf_p_pub = rospy.Publisher("/foot/pose_abs/rf", Point, queue_size=1)
-    lh_p_pub = rospy.Publisher("/foot/pose_abs/lh", Point, queue_size=1)
-    rh_p_pub = rospy.Publisher("/foot/pose_abs/rh", Point, queue_size=1)
+    lf_p_pub = rospy.Publisher("/foot/pose_rel/lf", Point, queue_size=1)
+    rf_p_pub = rospy.Publisher("/foot/pose_rel/rf", Point, queue_size=1)
+    lh_p_pub = rospy.Publisher("/foot/pose_rel/lh", Point, queue_size=1)
+    rh_p_pub = rospy.Publisher("/foot/pose_rel/rh", Point, queue_size=1)
   
-    rate = rospy.Rate(100)
+    rate = rospy.Rate(5)
 
     body_x = 0
     body_y = 0
@@ -40,15 +40,8 @@ if __name__ == '__main__':
     body_P = 0
     body_Y = 0
 
-    body_dx = 0.0001
-    body_dy = 0.0001
-    body_dz = 0
-    body_dR = 0
-    body_dP = 0
-    body_dY = 0
-
-    lf_fx = 0.038
-    lf_fy = -0.12
+    lf_fx = 0
+    lf_fy = 0
     lf_fz = 0
 
     lf_fdx = 0
@@ -67,34 +60,25 @@ if __name__ == '__main__':
     rh_fy = 0
     rh_fz = 0
 
+    counter = 0
+
     try:
         while not rospy.is_shutdown():
-            body_msg = generate_pose_msg(body_dx, body_dy, body_dz, body_dR, body_dP, body_dY)
+            if counter <= 10:
+                rospy.loginfo("published!!")
+                body_msg = generate_pose_msg(0, 0.002, 0, 0, 0, 0)
 
-            body_x += body_dx
-            body_y += body_dy
-            body_z += body_dz
+                lf_msg = generate_point_msg(0, 0, 0)
+                rf_msg = generate_point_msg(0, 0.002, 0)
+                lh_msg = generate_point_msg(0, 0, 0)
+                rh_msg = generate_point_msg(0, 0, 0)
 
-            if (body_x > 0.05):
-                lf_fdy = 0.0001
-                body_dx = 0
-                body_dy = 0
-
-            lf_msg = generate_point_msg(0, 0, 0)
-            rf_msg = generate_point_msg(lf_fdx, lf_fdy, lf_fdz)
-            lh_msg = generate_point_msg(lh_fx, lh_fy, lh_fz)
-            rh_msg = generate_point_msg(rh_fx, rh_fy, rh_fz)
-
-            lf_fx += lf_fdx
-            lf_fy += lf_fdy
-            lf_fz += lf_fdz
-
-            body_p_pub.publish(body_msg)
-            lf_p_pub.publish(lf_msg)
-            rf_p_pub.publish(rf_msg)
-            lh_p_pub.publish(lh_msg)
-            rh_p_pub.publish(rh_msg)
-
+                body_p_pub.publish(body_msg)
+                lf_p_pub.publish(lf_msg)
+                rf_p_pub.publish(rf_msg)
+                lh_p_pub.publish(lh_msg)
+                rh_p_pub.publish(rh_msg)
+                counter += 1
             rate.sleep()
             
     except rospy.ROSInterruptException:
